a2bb5c76e5fe155e7787034bbe8f591aeb2d96c6
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-14 17:55:17 +0800

    Update header

--- packages/informatif.netlify.app/src/App.js
+++ packages/informatif.netlify.app/src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import StackOverflowList from "./components/StackOverflowList";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app">
+    <header className="header">
+      <h1 className="title">Informatif</h1>
+      <nav>
+        <ul className="nav">
+          <li>
+            <a href="#" className="nav__link">
+              Stack Overflow
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <StackOverflowList />
+  </div>
+);
 
 export default App;
