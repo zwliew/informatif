21ff16dfdfd5d045572650100e3b4fd71d7f55b0
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-17 19:14:54 +0800

    Set up compression and helmet

--- packages/informatif-api/app/index.mjs
+++ packages/informatif-api/app/index.mjs
@@ -1,8 +1,12 @@
 import Koa from "koa";
+import compress from "koa-compress";
+import helmet from "koa-helmet";
 import router from "./routes";
 
 const app = new Koa();
 
+app.use(compress());
+app.use(helmet());
 app.use(router.routes());
 
 export default app;
