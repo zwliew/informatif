24c50f150d115bfa6f55c96183b1ae5c33268a90
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-19 10:12:07 +0800

    Remove unnecessary StrictMode usage
    Concurrent mode enforces StrictMode by default.

--- packages/informatif.netlify.app/src/index.js
+++ packages/informatif.netlify.app/src/index.js
@@ -1,14 +1,10 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { unstable_createRoot } from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-unstable_createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+unstable_createRoot(document.getElementById("root")).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
