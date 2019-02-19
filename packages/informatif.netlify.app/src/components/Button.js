179852f0750d40188271273c353f0fbb8cc895c7
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-19 21:54:38 +0800

    Invert colors of button on hover

--- packages/informatif.netlify.app/src/components/Button.js
+++ packages/informatif.netlify.app/src/components/Button.js
@@ -2,14 +2,17 @@ import styled from "styled-components/macro";
 
 export default styled.button`
   align-items: center;
-  background: none;
+  background-color: var(--background-color);
   border: 1px solid var(--text-color);
   border-radius: 4px;
   color: var(--text-color);
   display: inline-flex;
   padding: 4px;
+  transition: background-color 80ms ease-out;
 
   :hover {
+    background-color: var(--text-color);
+    color: var(--background-color);
     cursor: pointer;
   }
 `;
