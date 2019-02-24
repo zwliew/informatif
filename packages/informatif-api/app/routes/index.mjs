60d7c7151b1422a536e5745e1abecb29650e5895
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-24 22:16:21 +0800

    Set cache control header

--- packages/informatif-api/app/routes/index.mjs
+++ packages/informatif-api/app/routes/index.mjs
@@ -4,6 +4,10 @@ import auth from "./auth";
 
 const router = new Router();
 
+router.use(async (ctx, next) => {
+  await next();
+  ctx.set("Cache-Control", "public,max-age=1800");
+});
 router.use(auth.routes()).use(api.routes());
 
 export default router;
