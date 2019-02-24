e8249e5467c820ca640f97d46c7da7757aa05000
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-02-24 21:28:26 +0800

    Improve unstable_createRoot type definition

--- packages/informatif.netlify.app/src/types/react-dom.d.ts
+++ packages/informatif.netlify.app/src/types/react-dom.d.ts
@@ -1,3 +1,6 @@
 declare module "react-dom" {
-  export function unstable_createRoot(el: HTMLElement | null);
+  export function unstable_createRoot(
+    container: HTMLElement | null,
+    options?: { hydrate?: boolean }
+  ): { render: (element: JSX.Element) => void };
 }
