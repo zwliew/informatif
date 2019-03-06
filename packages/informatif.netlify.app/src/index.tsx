d01338d524395022876bdce271242c0c4e545431
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-03-06 10:53:23 +0800

    Install react-a11y

--- packages/informatif.netlify.app/src/index.tsx
+++ packages/informatif.netlify.app/src/index.tsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { unstable_createRoot } from "react-dom";
+import ReactDOM, { unstable_createRoot } from "react-dom";
 import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+
+if (process.env.NODE_ENV === "development") {
+  const a11y = require("react-a11y").default;
+  a11y(React, ReactDOM, {
+    rules: {
+      "aria-role": "warn",
+      "aria-unsupported-elements": "warn",
+      "click-events-have-key-events": "warn",
+      "hidden-uses-tabindex": "warn",
+      "img-redundant-alt": "warn",
+      "img-uses-alt": "warn",
+      "interactive-supports-focus": "warn",
+      "label-has-for": "warn",
+      "mouse-events-have-key-events": "warn",
+      "no-access-key": "warn",
+      "no-hash-ref": "warn",
+      "no-onchange": "warn",
+      "onclick-uses-role": "warn",
+      "tabindex-no-positive": "warn",
+      "tabindex-uses-button": "warn"
+    }
+  });
+}
 
 unstable_createRoot(document.getElementById("root")).render(<App />);
 
