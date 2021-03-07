5989a15075dac0bd4774d53aee194de3641bed26
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-07 23:46:13 +0800

    Update some deps and use stable react API

--- website/src/types/react-dom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "react-dom" {
-  export function unstable_createRoot(
-    container: HTMLElement | null,
-    options?: { hydrate?: boolean }
-  ): { render: (element: JSX.Element) => void };
-}
